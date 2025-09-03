04ed32be1eac3755bf18f7c01871ddab98aa8e40
Level222 <124219660+Level222@users.noreply.github.com>
2025-09-03 16:04:27 +0900

    画像やアニメーションを追加

--- script.js
+++ script.js
@@ -6,4 +6,30 @@ const JST = +9;
 const eventTimestamp = Date.UTC(2025, 8, 20, -JST);
 const remainingDays = Math.ceil((eventTimestamp - Date.now()) / (24 * 60 * 60 * 1000));
 
-counter.textContent = `あと${remainingDays}日`;
+if (remainingDays > 0) {
+  counter.textContent = `あと${remainingDays}日`;
+} else if (remainingDays === 0) {
+  counter.textContent = '本日開催！';
+} else {
+  counter.textContent = '開催終了';
+}
+
+const moonPhase = document.getElementById('moon-phase');
+
+const createMoonPhasePath = (phase, r) => {
+  const semicircle = `a ${r} ${r} 0 0 ${Number(phase % 1 < 0.5)} 0 ${r * 2}`;
+  const semiEllipse = `a ${r * Math.abs(Math.cos(2 * Math.PI * phase))} ${r} 0 0 ${Number(phase % 0.5 > 0.25)} 0 ${-r * 2}`;
+  return `${semicircle} ${semiEllipse}`;
+};
+
+const updateMoonPhase = () => {
+  // トップ画像の部分を除く
+  const minScrollY = document.documentElement.clientHeight;
+  const maxScrollY = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const phase = Math.min(Math.max(window.scrollY - minScrollY, 0), maxScrollY) / (maxScrollY - minScrollY);
+
+  moonPhase.setAttribute('d', `M 64 0 ${createMoonPhasePath(phase, 64)} Z`);
+};
+
+updateMoonPhase();
+window.addEventListener('scroll', updateMoonPhase);
